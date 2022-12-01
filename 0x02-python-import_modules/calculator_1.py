303f019b9f01f286e7443434e8226e8ae83d1803
Macaire <macaire.ngue@outlook.com>
2022-12-01 04:56:53 -0800

    Project Python - import & modules

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def add(a,b):
-    """My addition function,
-
+def add(a, b):
+    """My addition function
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a + b
     """
@@ -14,11 +12,9 @@ def add(a,b):
 
 def sub(a, b):
     """My subtraction function
-
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a - b
     """
@@ -27,24 +23,20 @@ def sub(a, b):
 
 def mul(a, b):
     """My multiplication function
-
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a * b
     """
     return (a * b)
 
 
-def div(a, b)
+def div(a, b):
     """My division function
-
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a / b
     """
