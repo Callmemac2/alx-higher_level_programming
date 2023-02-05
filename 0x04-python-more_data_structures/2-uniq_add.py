9168da1bb51935d33d1164f769eac50a08c3f143
Macaire <macaire.ngue@outlook.com>
2023-02-05 10:14:52 -0800

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    number = 0
-    for element in set(my_list):
-        number += element
-    return number
+    resultat = sum(set(my_list))
+    return resultat
